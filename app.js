faed43db604eda93f4db2179e764a73f2bb7c1d5
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-06-22 16:32:01 +0530

    fetch single tour

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
+  }
+
+  res.json({
+    status: 200,
+    data: { tour },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;

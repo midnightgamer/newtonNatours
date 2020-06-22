8fe0047ec64d3ed1f2167ecb2709ad16b308114a
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-06-22 16:18:58 +0530

    post route added to add new tours

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { readFileSync } = require('fs');
+const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
+
 let tours = JSON.parse(
-  readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 app.get('/api/v1/tours', (req, res) => {
@@ -13,6 +15,26 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length,
     data: { tours },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+
+  const newTours = Object.assign({ id: newId }, req.body);
+  tours.push(newTours);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).send({
+        status: '200',
+        data: {
+          tours: newTours,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;

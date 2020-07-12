eb17ee8a1402756bf529cf99aea56e5a5ec72042
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-07-12 22:00:22 +0530

    mongoose added

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
-const port = process.env.PORT;
+const mongoose = require('mongoose');
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+   '<password>',
+   process.env.DBPASSWORD
+);
+mongoose
+   .connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+   })
+   .then(() => {
+      console.info('DB Connected');
+   });
+
+const port = process.env.PORT;
 app.listen(port, () => {
-   console.log(`Server listening on http://localhost:${port}`);
+   console.log(
+      `Server listening on http://localhost:${port}`
+   );
 });

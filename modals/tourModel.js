fc94d2d24a3f2031b51db216b6eb4b786c2cae48
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-07-15 12:20:14 +0530

    DOCUMENT MIDDLEWARE added

--- modals/tourModel.js
+++ modals/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
    {
@@ -6,6 +7,9 @@ const tourSchema = new mongoose.Schema(
          type: String,
          required: true,
          unique: true,
+      },
+      slug: {
+         type: String,
       },
       duration: {
          type: Number,
@@ -64,6 +68,19 @@ const tourSchema = new mongoose.Schema(
       },
    }
 );
+
+//DOCUMENT MIDDLEWARE: pre runs before .save and .create
+tourSchema.pre('save', function (next) {
+   this.slug = slugify(this.name, {
+      lower: true,
+   });
+   next();
+});
+//DOCUMENT MIDDLEWARE: post runs after .save and .create
+/*tourSchema.post('save', function (doc, next) {
+   console.log(doc);
+   next();
+});*/
 //Virtual Properties
 tourSchema.virtual('durationWeek').get(function () {
    return this.duration / 7;

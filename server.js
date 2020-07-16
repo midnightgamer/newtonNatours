f1232f582ee47597a46045ae492956093b62dc4f
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-07-16 13:52:02 +0530

    global unhadle promis handler added

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ mongoose
    .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
    console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+   console.log(err.name, err.message);
+   console.error('Unhandled rejection , Shutting down');
+   server.close(() => {
+      process.exit(1);
+   });
 });

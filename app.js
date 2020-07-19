b8ab1c5ab5098f27a3100abb911d23ab795b7f12
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-07-19 22:16:05 +0530

    two packages added for security mesures

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const AppError = require('./utils/appError');
 
 const globalErrors = require('./controllers/errorController');
@@ -27,6 +29,12 @@ app.use(helmet());
 app.use('/api', limiter);
 //Body parser rading data from data.req
 app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NOSQL query injection
+app.use(mongoSanitize());
+//Data sanitization against xss
+app.use(xss());
+
 //Static file serving
 app.use(express.static(`${__dirname}/public`));
 

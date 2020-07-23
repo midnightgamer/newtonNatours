4f1f17ac9b761e2bce9c4d0d6900e497171eaac8
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-07-23 16:34:05 +0530

    new route to get logged in user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,6 +7,7 @@ const {
    updateMe,
    getAllUsers,
    deleteMe,
+   getMe,
 } = require('../controllers/userController');
 const { protect } = require('../controllers/authController');
 
@@ -14,6 +15,7 @@ const router = express.Router();
 
 router.route('/').get(getAllUsers).post(createUser);
 
+router.route('/me').get(protect, getMe, getUser);
 router.route('/updateMe').patch(protect, updateMe);
 router.route('/deleteMe').delete(protect, deleteMe);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);

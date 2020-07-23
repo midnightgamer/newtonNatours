a4fb8cb6272f346cbcbff2669ff2e5e8408f30e1
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-07-23 16:41:20 +0530

    protected route user using middleware function

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -12,12 +12,12 @@ const {
 const { protect } = require('../controllers/authController');
 
 const router = express.Router();
-
+router.use(protect);
 router.route('/').get(getAllUsers).post(createUser);
 
-router.route('/me').get(protect, getMe, getUser);
-router.route('/updateMe').patch(protect, updateMe);
-router.route('/deleteMe').delete(protect, deleteMe);
+router.route('/me').get(getMe, getUser);
+router.route('/updateMe').patch(updateMe);
+router.route('/deleteMe').delete(deleteMe);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;

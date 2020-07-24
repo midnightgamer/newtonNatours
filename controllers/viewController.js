310c08f659e2cdac0adc091436ddf0d0ce08b0a7
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-07-24 21:03:44 +0530

    overivew page added

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,11 +1,18 @@
+const Tour = require('../modals/tourModel');
+const catchAsymc = require('../utils/catchAsync');
+
+exports.getOverview = catchAsymc(async (req, res, next) => {
+   // 1 Get tour data from collection
+   const tours = await Tour.find();
+   // 2 Build template
+   // 3 Render template
+   res.status(200).render('overview', {
+      tours,
+   });
+});
+
 exports.getTour = (req, res) => {
    res.status(200).render('tour', {
       title: 'Tour',
-   });
-};
-
-exports.getOverview = (req, res) => {
-   res.status(200).render('overview', {
-      title: 'Overview',
    });
 };

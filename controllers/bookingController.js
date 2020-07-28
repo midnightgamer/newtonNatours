13593382773bc9a1aa1a0e71a0e5994165fba60e
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-07-28 20:18:26 +0530

    courses completed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,8 +1,8 @@
+const factory = require('./factoryController');
 const Tour = require('../modals/tourModel');
 const Booking = require('../modals/bookingModal');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-// const factory = require('factoryController.js');
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
@@ -38,6 +38,16 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 exports.createBookingCheckout = catchAsync(async (req, res, next) => {
    const { tour, user, price } = req.query;
    if (!tour && !user && !price) return next();
-   await Booking.create({ tour, user, price });
+   await Booking.create({
+      tour,
+      user,
+      price,
+   });
    res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBooking = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);

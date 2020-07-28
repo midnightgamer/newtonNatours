26393437ebcfe88e389828b3ab520904807fd1c9
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-07-28 19:01:29 +0530

    storing booked tour in bd

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,4 +1,5 @@
 const Tour = require('../modals/tourModel');
+const Booking = require('../modals/bookingModal');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 // const factory = require('factoryController.js');
@@ -10,7 +11,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
    //    2 Create Checkout session
    const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
-      success_url: `${req.protocol}://${req.get('host')}/`,
+      success_url: `${req.protocol}://${req.get('host')}/?tour=${
+         req.params.tourID
+      }&user=${req.user.id}&price=${tour.price}`,
       cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
       customer_email: req.user.email,
       client_reference_id: req.params.tourID,
@@ -30,4 +33,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       status: 'success',
       session,
    });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+   const { tour, user, price } = req.query;
+   if (!tour && !user && !price) return next();
+   await Booking.create({ tour, user, price });
+   res.redirect(req.originalUrl.split('?')[0]);
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,11 +5,12 @@ const {
    getLogin,
    updateUserData,
 } = require('../controllers/viewController');
+const { createBookingCheckout } = require('../controllers/bookingController');
 
 const { isLoggedIn, protect } = require('../controllers/authController.js');
 
 const router = express.Router();
-router.get('/', isLoggedIn, getOverview);
+router.get('/', createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLogin);
 router.get('/me', protect, updateUserData);

d094c093f4aff88ca9764937d328bd409a1a2986
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-08-03 21:56:27 +0530

    view route hide

--- app.js
+++ app.js
@@ -90,13 +90,12 @@ app.use((req, res, next) => {
       express.static(path.join(__dirname, 'client', 'build', 'index.html'))
    );
 } */
-
 if (process.env.NODE_ENV === 'production') {
    //    set static assets folder
-   app.use(express.static('client/build'));
-
+   app.use('/', express.static('public'));
+   app.use(express.static(path.join(__dirname, 'client/build')));
    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client/build/index.html'));
    });
 }
 

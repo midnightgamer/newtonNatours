498d48aaf3564ef124fb51d2380b1e0b7f993484
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-08-03 12:10:50 +0530

    public folder added

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use((req, res, next) => {
 
 if (process.env.NODE_ENV === 'production') {
    //    set static assets folder
-   app.use(express.static('client/build'));
+   app.use(express.static('public'));
 
    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));

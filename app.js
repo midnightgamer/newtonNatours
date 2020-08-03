e9bb05a7dd7135cbdb9c566dafd9c46801124485
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-08-03 11:48:37 +0530

    client set as static folder

--- app.js
+++ app.js
@@ -83,6 +83,15 @@ app.use((req, res, next) => {
    next();
 });
 
+if (process.env.NODE_ENV === 'production') {
+   //    set static assets folder
+   app.use(express.static('client/build'));
+
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   });
+}
+
 // // 3) ROUTES
 // app.use('/', cors(), viewRouter);
 app.use('/api/v1/tours', tourRouter);

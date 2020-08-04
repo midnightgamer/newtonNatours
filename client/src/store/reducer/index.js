1125375d82aa0961d8a6053e10900a30689e9955
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-08-04 17:07:12 +0530

    stripe integrtion check3

--- client/src/store/reducer/index.js
+++ client/src/store/reducer/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import alert from './alert';
 import profile from './profile';
+import booking from './booking';
 import tours from './tours';
 
 export default combineReducers({
    auth,
    profile,
+   booking,
    tours,
    alert,
 });

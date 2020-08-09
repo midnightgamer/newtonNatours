f7d87f4754642aabbb49d7f0c4fac6999257b5ba
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-08-09 16:03:04 +0530

    booking tour check 1

--- client/src/store/action/booking.js
+++ client/src/store/action/booking.js
@@ -3,10 +3,10 @@ import { setAlert } from './alert';
 import { BOOK_TOUR } from './types';
 import stripe from 'stripe';
 export const bookTour = (tourId) => async (dispatch) => {
-   const Strip = stripe(
-      'sk_test_51H9VXXH8UAzj9LgCu81692Nlh1DYWonHKF8swHNf6b8H0voxPNNpuLkfTQ4urLTboqQ57j3a2NgaYKZrctjXMrLk00hPMqwZ8z'
-   );
    try {
+      const Strip = stripe(
+         'sk_test_51H9VXXH8UAzj9LgCu81692Nlh1DYWonHKF8swHNf6b8H0voxPNNpuLkfTQ4urLTboqQ57j3a2NgaYKZrctjXMrLk00hPMqwZ8z'
+      );
       //   1) Get checkout session from the server
       const session = await axiosInstance(
          `/bookings/checkout-session/${tourId}`
@@ -20,6 +20,7 @@ export const bookTour = (tourId) => async (dispatch) => {
          payload: session,
       });
    } catch (e) {
+      console.log(e);
       setAlert('error', e);
    }
 };
